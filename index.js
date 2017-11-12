e31706fbe17a976f883a9ea9e0bbfdc8f0a974c5
matus <bartko.mat@gmail.com>
2017-11-12 23:40:23 +0100

    test pipeline

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
+    console.log("teest");
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
