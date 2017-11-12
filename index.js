4ab4ad31500a9491adba16530b00035221286c19
matus <bartko.mat@gmail.com>
2017-11-12 22:12:43 +0100

    init by http://docs.aws.amazon.com/lambda/latest/dg/automating-deployment.html

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by matus on 12.11.2017.
+ */
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date();
+    currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    });
+};
